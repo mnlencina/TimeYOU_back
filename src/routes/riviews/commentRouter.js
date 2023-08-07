84f9ae29c5a8ea5412e543e73c8a3e044e224816
mnlencina <mnlencina@gmail.com>
2023-08-07 19:14:40 -0300

    update

--- src/routes/riviews/commentRouter.js
+++ src/routes/riviews/commentRouter.js
@@ -7,22 +7,27 @@ const commentRouter = express.Router();
 // POST route to create a new comment
 commentRouter.post("/", async (req, res) => {
 try {
-    const { watchId, commentText, calification } = req.body;
-    const userId = req.user.id; // Assuming you have the user information in the request
+    const { userId, watchId, commentText, calification, userName } = req.body;
+
+   // console.log(req.body)
+    
 
     // Ensure watchId is a valid watch in the database
     const watch = await Watch.findOne({ where: { id: watchId } });
-    if (!watch) {
-    return res.status(404).json({ error: "Watch not found." });
+    const user = await User.findOne({where: {id: userId}})
+    if (!watch || !user) {
+    return res.status(404).json({ error: "Watch or User not found." });
     }
 
     // Create the new comment with the associated user
-    const newComment = await createComment(watchId, commentText, calification, userId);
+    const newComment = await createComment(userId, watchId, commentText, calification, userName);
     res.status(201).json(newComment);
 } catch (error) {
     res.status(500).json({ Error: error.message });
 }
 });
+
+
 
 // GET route to fetch comments by watch Id
 commentRouter.get("/watch/:watchId", async (req, res) => {
@@ -38,17 +43,38 @@ try {
 }
 });
 
+
+commentRouter.get("/:userId", async (req, res)=> {
+    const {userId } = req.params;
+    try {
+        const userId = await User.findOne({
+            where: {UserId: userId},
+            include: Comment,
+        });
+        res.status(200).json(userId)
+    } catch (error) {
+        res.status(500).json({ Error: error.message });
+    }
+})
+
+//GET comment from 1 user for 1 watch
+commentRouter.get("/:watchId/:userId", async(req,res) => {
+    const { watchId , userId } = req.params;
+    try {
+        const allComments = await Comment.findAll({
+            where: {WatchId: watchId, UserId: userId},
+            include:{model: User},
+            include:{model: Watch},
+        });
+        res.status(200).json(allComments);
+    } catch (error) {
+        res.status(500).json({ Error: error.message });
+    }
+})
+
+
+
 module.exports = commentRouter;
-
-
-
-
-
-
-
-
-
-
 
 
 
--- src/service/socket.js
+++ src/service/socket.js
@@ -2,7 +2,7 @@ import { Server } from "socket.io";
 
 const io = new Server({
     cors:{
-        origin: "http://localhost:3001/"
+        origin: "https://timeyouback.up.railway.app/"
     }
 });
 
